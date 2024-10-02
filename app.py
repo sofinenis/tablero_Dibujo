f62963bdf8e76026f15acf0b0093e22f2478f844
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-02 16:59:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ with st.sidebar:
 st.subheader("Dibuja el boceto en el panel  y presiona el botón para analizarla")
 
 # Add canvas component
-bg_image = st.sidebar.file_uploader("Cargar Imagen:", type=["png", "jpg"])
+#bg_image = st.sidebar.file_uploader("Cargar Imagen:", type=["png", "jpg"])
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
 stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 5)
@@ -55,7 +55,7 @@ canvas_result = st_canvas(
     background_color=bg_color,
     height=300,
     width=400,
-    background_image=Image.open(bg_image) if bg_image else None,
+    background_image= None #Image.open(bg_image) if bg_image else None,
     drawing_mode=drawing_mode,
     key="canvas",
 )
